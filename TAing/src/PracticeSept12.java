1112294df052ed054c525f6f3565fd5ab051eb66
Nick-AI <31321017+Nick-AI@users.noreply.github.com>
2018-09-13 01:07:26 -1000

    Add files via upload

---
+++ TAing/src/PracticeSept12.java
@@ -0,0 +1,38 @@
+public class PracticeSept12 {
+
+    public static void main(String[] args){
+        /*
+         * Write a while loop to count from -3 to 210
+         * Print out all even numbers in the format "[number] is even"
+         * Also print out all of the numbers divisible by seven
+         */
+
+        /*
+         * Write a while loop that counts back from 90 to -90
+         * Always print the number if it is divisible by 9
+         * Always print the number if it is a multiple of 5
+         * If the number is even, print "EVEN!", but only if it is a negative, otherwise print "negative even"
+         * If the number is odd, print "[number] is not even"
+         */
+
+        /*
+         * Calculate the area of a triangle with the lengths 16 and 9243 using your function and print the result
+         */
+
+        /*
+         * For the following three test score print out "[test score] is equal to a grade of [calculated grade]" using
+         * the function you wrote below.
+         * Scores: 10, 99, 82, 1000, -1
+         */
+
+    }
+
+    /*
+     * Write a function to calculate the area of a right traingle given the length of its two legs
+     */
+
+    /*
+     * Write a function that takes in a number between 0 and 100 and prints out the appropriate grade
+     * If a number outside of that range is given, your function has to handle it appropriately
+     */
+}
