440a464e49a74e393ae4079c809d7cace86dfe65
Nick-AI <31321017+Nick-AI@users.noreply.github.com>
2018-09-20 01:08:21 -1000

    Update PracticeSpt18.java

--- TAing/src/PracticeSpt18.java
+++ TAing/src/PracticeSpt18.java
@@ -4,15 +4,28 @@ public class PracticeSpt18 {
 
 	public static void main(String[] args){
 		//write a while loop that prints all numbers from 1 to 100. If the number has only one digit, also print "low" on the same line as the number, otherwise print "high"
+		int count = 1;
+		while(count<=100){
+			if(count<10)
+				System.out.println(count + " low");
+			else
+				System.out.println(count + " high");
+			count++;
+		}
+		printStudent("Bob", 18);
 		clickCar();
 	}
 	
 	//complete the function header so it takes in the name of the student and his/ her age. 
-	public static void printStudent(){
+	public static void printStudent(String name, int age){
 		boolean isAdult;
 		//write an if statement to determine whether isAdult is true (student is at least 18) or false (student is under 18)
-		
+		isAdult = (age>17);
 		//print out the name of the student and whether he/ she is an adult or not based on the value of isAdult
+		if(isAdult)
+			System.out.println(name + " is an adult.");
+		else
+			System.out.println(name + " is not an adult.");
 	}
 	
 	//write a function to create an EZ window with a car that always drives toward where you click with the mouse, if it reaches an edge it stop until you click again. 
