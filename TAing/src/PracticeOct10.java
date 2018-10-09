8fcab7e90bd02e53636a6e2289afc2c5e4a7d0ec
Nick-AI <31321017+Nick-AI@users.noreply.github.com>
2018-10-09 00:21:33 -1000

    Create PracticeOct10.java

---
+++ TAing/src/PracticeOct10.java
@@ -0,0 +1,22 @@
+public class PracticeOct10 {
+
+	public static void main(String[] args){
+		//download the Student and CollegeClass classes, you'll need them
+		
+		//creating a new object of the class CollegeClass
+		CollegeClass ics111 = new CollegeClass();
+		
+		//specify the name of the class (Intro to Java/ Computer Science/ Programming?) and how many credits its worth
+		
+		//set the maximum size of the class
+		
+		//Complete the Student class
+		Student s = new Student();
+		
+		//set the student's name and student id
+		
+		//add the student to your class 
+		
+		//print out the class list
+	}
+}
