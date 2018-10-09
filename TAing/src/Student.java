f755281d8c2b15023d74a8a9c6045b228da0e11b
Nick-AI <31321017+Nick-AI@users.noreply.github.com>
2018-10-09 00:13:49 -1000

    Create Student.java

---
+++ TAing/src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+	
+	//instance variables/ member variables should include name, and student id number
+	
+	//methods should enable to specify student name, student id number
+	//also methods to retrieve the student's name and id number
+
+}
