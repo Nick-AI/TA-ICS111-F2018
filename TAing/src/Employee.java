89bf5e69915603755b46dcd6e53fa7ac542dabf2
Nick-AI <31321017+Nick-AI@users.noreply.github.com>
2018-10-11 00:52:35 -1000

    Create Employee.java

---
+++ TAing/src/Employee.java
@@ -0,0 +1,8 @@
+public class Employee {
+
+    //add three instance variables: name, salary, and employee id
+
+    //create a constructor that initializes the name and employee id
+
+    //create a method that sets the salary
+}
