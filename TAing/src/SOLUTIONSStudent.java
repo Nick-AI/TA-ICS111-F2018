3018edae64c69a1631db09bd81a412f0ce8a169d
Nick-AI <31321017+Nick-AI@users.noreply.github.com>
2018-10-11 00:42:46 -1000

    Create SOLUTIONSStudent.java

---
+++ TAing/src/SOLUTIONSStudent.java
@@ -0,0 +1,24 @@
+public class SOLUTIONSStudent {
+
+    //instance variables/ member variables should include name, and student id number
+    String name;
+    int idNumber;
+    //methods should enable to specify student name, student id number
+    //also methods to retrieve the student's name and id number
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int id) {
+        idNumber = id;
+    }
+}
