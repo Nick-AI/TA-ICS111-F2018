7604d375bed200877cb8da1d6c72f4412fb69d87
Nick-AI <31321017+Nick-AI@users.noreply.github.com>
2018-10-16 00:56:47 -1000

    Update Employee.java

--- TAing/src/Employee.java
+++ TAing/src/Employee.java
@@ -1,8 +1,16 @@
 public class Employee {
 
     //add three instance variables: name, salary, and employee id
-
+	String name;
+	double salary;
+	int employeeID;
     //create a constructor that initializes the name and employee id
-
+	public Employee(String n, int id){
+		name = n;
+		employeeID = id;
+	}
     //create a method that sets the salary
+	public void setSalary(double sal){
+		salary = sal;
+	}
 }
